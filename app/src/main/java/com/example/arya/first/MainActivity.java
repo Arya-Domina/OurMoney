edc75a1e7cb892b4aa1ead887e9befbd97c4ffbe
Arya-Domina <aryadomina@gmail.com>
2016-12-03 00:14:06 +0600

    "test cool"

--- app/src/main/java/com/example/arya/first/MainActivity.java
+++ app/src/main/java/com/example/arya/first/MainActivity.java
@@ -1,40 +1,63 @@
 package com.example.arya.first;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.TabActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TabHost;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
 
-    private TextView testTextView;
-    private EditText testEditText;
-    private Button testButton;
+    private TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        testTextView = (TextView)findViewById(R.id.textView);
-        testEditText = (EditText)findViewById(R.id.test_edit_text);
-        testButton = (Button)findViewById(R.id.test_button);
+//        tabHost = (TabHost)findViewById(R.id.tabhost);
+//        tabHost.setup();
+        tabHost = getTabHost();
+        TabHost.TabSpec tabSpec;
 
-        testButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                testTextView.setText(testEditText.getText());
+        tabSpec = tabHost.newTabSpec("tag1");
+        tabSpec.setIndicator("Taaab1!");
+        tabSpec.setContent(TabFactory);
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag2");
+        tabSpec.setIndicator(null, getResources().getDrawable(R.drawable.tab_icon_selector));
+        tabSpec.setContent(TabFactory);
+        tabHost.addTab(tabSpec);
+/*
+        tabSpec = tabHost.newTabSpec("tag3");
+        tabSpec.setIndicator("Taaab3!");
+//        View view = getLayoutInflater().inflate(R.layout.activity_history, null);
+//        tabSpec.setIndicator(view);
+        tabSpec.setContent(R.id.tab_text3);
+        tabHost.addTab(tabSpec);
+
+        tabHost.setCurrentTabByTag("tag2");
+
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            public void onTabChanged(String tabId) {
+                Toast.makeText(getBaseContext(), "tabId = " + tabId, Toast.LENGTH_SHORT).show();
             }
-        });
-
+        });*/
     }
 
-    public void onClick(View view) {
-//        Intent intent = new Intent(MainActivity.this, TabLayoutActivity.class);
-//        startActivity(intent);
-    }
+    TabHost.TabContentFactory TabFactory = new TabHost.TabContentFactory() {
+        @Override
+        public View createTabContent(String tag) {
+            if (tag == "tag1") {
+                return getLayoutInflater().inflate(R.layout.tab, null);
+            } else if (tag == "tag2") {
+                TextView textView = new TextView(MainActivity.this);
+                textView.setText("omnomnom");
+                return textView;
+            }
+            return null;
+        }
+    };
 
 }
---
+++ app/src/main/java/com/example/arya/first/MyItemsActivity.java
@@ -0,0 +1,16 @@
+package com.example.arya.first;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MyItemsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_items);
+    }
+}
