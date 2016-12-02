6cb9ae84aac76e48a438b96aa213c8aed5435901
Arya-Domina <aryadomina@gmail.com>
2016-12-02 18:13:31 +0600

    "clear"

--- app/src/main/java/com/example/arya/first/MainActivity.java
+++ app/src/main/java/com/example/arya/first/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        Intent intent = new Intent(MainActivity.this, TabLayoutActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(MainActivity.this, TabLayoutActivity.class);
+//        startActivity(intent);
     }
 
 }
